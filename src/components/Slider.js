de43ef6e9babca2ec4386e3dcc2a89fbd261f0a3
Rahim Uddin Rafin <rahim.rafin.1@gmail.com>
2023-04-18 15:24:03 +0600

    bug-fixed

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,9 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 // // Images
-import bg01 from '../assets/images/bg01.jpg';
-import bg02 from '../assets/images/bg02.jpg';
-import bg03 from '../assets/images/bg03.jpg';
+import bg01 from "../assets/images/bg01.jpg";
+import bg02 from "../assets/images/bg02.jpg";
+import bg03 from "../assets/images/bg03.jpg";
 let index = 0;
 
 export default function Slider() {
@@ -16,17 +16,23 @@ export default function Slider() {
     let slides = [bg01, bg02, bg03];
     const interval = setInterval(() => {
       if (index >= 3) index = 0;
-      setSlide(slides[index])
-      index += 1
+      setSlide(slides[index]);
+      index += 1;
     }, delay);
     return () => {
       clearInterval(interval);
-    }
-  }, [])
+    };
+  }, []);
 
   return (
     <div className="simpleslide100">
-      <div className="simpleslide100-item bg-img1" style={{ backgroundImage: `url(${slide})`, animation: 'fadeIn 7s infinite' }}></div>
+      <div
+        className="simpleslide100-item bg-img1"
+        style={{
+          backgroundImage: `url(${slide})`,
+          animation: "fadeIn 7s infinite",
+        }}
+      ></div>
     </div>
   );
 }
